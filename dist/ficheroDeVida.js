8beee091beed35255f28cbd7558b8cf2959bdcd3
kouski <72550619+kouski@users.noreply.github.com>
2023-11-16 19:13:10 +0100

    crear funcion limpiar contenido

--- dist/ficheroDeVida.js
+++ dist/ficheroDeVida.js
@@ -3,6 +3,7 @@ import { Paises } from "./clases/Paises.js";
 let paises = new Paises();
 let paisesRecibidos = [];
 let div = document.querySelector("#contenido");
+let contenido = document.querySelector('#contenido');
 /*paises.getDatos("https://restcountries.com/v3.1/region/europe").then(datos => {
     paisesRecibidos = datos
   
@@ -27,7 +28,7 @@ let div = document.querySelector("#contenido");
         let option = document.createElement("option");
         option.value = "0";
         option.text = "Seleciona Pais....";
-        h1.innerHTML = "Deber seleccionar un continente";
+        h1.innerHTML = "Debes seleccionar un continente";
         selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(option);
     }
     paises.getDatos("https://restcountries.com/v3.1/region/" +
@@ -48,23 +49,6 @@ let div = document.querySelector("#contenido");
                 option.text = (_d = (_c = pais.translations) === null || _c === void 0 ? void 0 : _c.spa) === null || _d === void 0 ? void 0 : _d.common;
             selectPaises === null || selectPaises === void 0 ? void 0 : selectPaises.appendChild(option);
         });
-        /* let btnGeneral = document.querySelector('#generales')
-
-        btnGeneral?.addEventListener('click', function(){
-          let paisSel =(selectPaises as HTMLSelectElement).selectedOptions[0].value
-          let contenido = document.querySelector('#contenido')
-          let h1Contenido = document.createElement('h1')
-
-          let paisSeleccionado =
-          h1Contenido.innerHTML = paisSel
-          contenido?.appendChild(h1Contenido)
-          
-          
-          
-          
-
-  }) */
-        // ... (código anterior)
         let btnGeneral = document.querySelector('#generales');
         let btnGeograficos = document.querySelector('#geograficos');
         let btnBanderas = document.querySelector('#banderas');
@@ -80,9 +64,7 @@ let div = document.querySelector("#contenido");
                 let enlace = `Nombre común:${paisSeleccionado.name.common}</br> Nombre Oficial: ${paisSeleccionado.name.official} </br>
         Capital:${paisSeleccionado.capital} </br> Población:${paisSeleccionado.population}`;
                 // Limpia el contenido actual antes de agregar el nuevo enlace
-                if (contenido) {
-                    contenido.innerHTML = "";
-                }
+                limpiarContenido();
                 // Agrega el nuevo enlace al elemento h1Contenido
                 h3Contenido.innerHTML = enlace;
                 contenido === null || contenido === void 0 ? void 0 : contenido.appendChild(h3Contenido);
@@ -99,9 +81,7 @@ let div = document.querySelector("#contenido");
                 let enlace = `Area:${paisSeleccionado.area}</br> Frontera: ${paisSeleccionado.borders} </br>
         <a href="${paisSeleccionado.googleMaps}" target="_blank">Google Maps:</a> </br> <a href="${paisSeleccionado.openStreetMaps}" target="_blank">Open Street Maps:</a>`;
                 // Limpia el contenido actual antes de agregar el nuevo enlace
-                if (contenido) {
-                    contenido.innerHTML = "";
-                }
+                limpiarContenido();
                 // Agrega el nuevo enlace al elemento h1Contenido
                 h3Contenido.innerHTML = enlace;
                 contenido === null || contenido === void 0 ? void 0 : contenido.appendChild(h3Contenido);
@@ -116,11 +96,10 @@ let div = document.querySelector("#contenido");
             if (paisSeleccionado) {
                 // Combina el nombre común y la capital en un enlace
                 let enlace = `Escudo: <img src="${paisSeleccionado.coatOfArms.svg}" width="100px" height="100px"/></br> 
-        Bandera: <img src="${paisSeleccionado.flags.svg}" width="100px" height="100px"/>`;
+        Bandera: <img src="${paisSeleccionado.flags.svg}" width="100px" height="100px"/>
+        `;
                 // Limpia el contenido actual antes de agregar el nuevo enlace
-                if (contenido) {
-                    contenido.innerHTML = "";
-                }
+                limpiarContenido();
                 // Agrega el nuevo enlace al elemento h1Contenido
                 h3Contenido.innerHTML = enlace;
                 h3Contenido.style.lineHeight = '4em';
@@ -137,9 +116,7 @@ let div = document.querySelector("#contenido");
                 // Combina el nombre común y la capital en un enlace
                 let enlace = `Traducción: ${paisSeleccionado.translations}`;
                 // Limpia el contenido actual antes de agregar el nuevo enlace
-                if (contenido) {
-                    contenido.innerHTML = "";
-                }
+                limpiarContenido();
                 // Agrega el nuevo enlace al elemento h1Contenido
                 h3Contenido.innerHTML = enlace;
                 h3Contenido.style.lineHeight = '4em';
@@ -148,3 +125,8 @@ let div = document.querySelector("#contenido");
         });
     });
 });
+function limpiarContenido() {
+    if (contenido) {
+        contenido.innerHTML = "";
+    }
+}
